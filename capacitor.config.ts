b94b8dd7dde5f7ec0ff9a1ce46cabd89b4a9ddc2
Csaba <todorcs@gmail.com>
2025-09-08 19:05:41 +0200

    revert: remove all splash screen and versioning changes to fix app crash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,20 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.hellodays.app',
   appName: 'HelloDays',
-  webDir: 'www',
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      backgroundColor: "#ffffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      androidTheme: "@style/AppTheme.Splash",
-      showSpinner: false,
-      splashFullScreen: true,
-      splashImmersive: true,
-    },
-  },
+  webDir: 'www'
 };
 
 export default config;
