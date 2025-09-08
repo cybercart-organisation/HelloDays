332389070009105a6b246ff7738dbb3acee2b2d8
Csaba <todorcs@gmail.com>
2025-09-08 10:58:36 +0200

    feat: add splash screen layout and resources for improved app branding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.hellodays.app',
   appName: 'HelloDays',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
